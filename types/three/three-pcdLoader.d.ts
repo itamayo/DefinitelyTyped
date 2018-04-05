ae3c9b7275970281c18159d142116e8524e94aa5
Iñigo Tamayo Uria <itamayo@vicomtech.org>
2018-04-05 14:26:31 +0200

    added typescript definition for three.pcloader

---
+++ types/three/three-pcdLoader.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="webvr-api" />
+
+import { Camera } from "./three-core";
+
+export class PCDLoader {
+    constructor();
+
+    /**
+     * Update pcdLoader Instance Point Cloud loader
+     */
+    load(url:String,onLoad:any,onProgress?:any,OnError?:any): void;
+
+}
